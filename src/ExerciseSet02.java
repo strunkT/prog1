db02baca83ef1f8d1c950ddd2691f500feea62ac
bircni <nicolas@bircks.eu>
2022-10-18 12:08:53 +0200

    formatted code

--- src/ExerciseSet02.java
+++ src/ExerciseSet02.java
@@ -1,53 +1,51 @@
 public class ExerciseSet02 {
-    public static int computeSum(int number) {
-        int result = 0; int i = 1;
+    public static int computeSum(int number)
+    {
+        int result = 0;
+        int i = 1;
         while (i <= number) {
-            result = result + i; i = i + 1;
+            result = result + i;
+            i = i + 1;
         }
         return result;
     }
-    public static void main(String[] args) {
 
-        /*  int num = Terminal.readInt("Provide a number");
-        System.out.println(computeSum(num));
+    /**
+     * make it easier
+     * before:
+     * int num6= Terminal.readInt("höhe");
+     * printTriangle(num6);
+     */
+    public static void main(String[] args)
+    {
 
-        int number1= Terminal.readInt("anzahl");
-        printSequence1(number1);
+        System.out.println(computeSum(Terminal.readInt("Provide a number")));
 
-        int num3=Terminal.readInt("anzahl");
-        printSequence2(num3);
+        printSequence1(Terminal.readInt("anzahl"));
 
-        int num4= Terminal.readInt("anzahl");
-        printSequence3(num4);
+        printSequence2(Terminal.readInt("anzahl"));
 
-        int num5= Terminal.readInt("höhe");
-        printTriangle(num5);
-        */
-        /**
-         * make it easier
-         */
+        printSequence3(Terminal.readInt("anzahl"));
+
         printTriangle(Terminal.readInt("hoehe"));
     }
     public static void printSequence1(int number1){
-    for (int i=1; i<= number1; i++)
-    System.out.println(i*i);
+    for (int i = 1; i <= number1; i++) System.out.println(i * i);
     }
+
     public static void printSequence2(int number2){
-        for (int i=1; i<= number2; i++)
-            if ((-(i)*1)%2==0)
-            System.out.println(-(i));
-        else
-                System.out.println(i);
+        for (int i = 1; i <= number2; i++)
+            if ((-(i)) % 2 == 0) System.out.println(-(i));
+            else System.out.println(i);
     }
+
     public static void printSequence3(int number3){
-        for (int i=1; i< number3; i++)
-            System.out.println((i*(i+1))/2);
+        for (int i = 1; i < number3; i++) System.out.println((i * (i + 1)) / 2);
     }
+
     public static void printTriangle(int height)
     {
+        for (int i = 0; i < height; i++) System.out.print(". ".repeat(i + 1).indent(height - i));
 
-        for (int i = 0; i < height; i++) {
-            System.out.print(". ".repeat(i + 1).indent(height - i));
-        }
     }
 }
