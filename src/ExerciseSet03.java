9355726963966812a106faf998fc0f141be3c4b1
Nicolas <75789103+bircni@users.noreply.github.com>
2022-10-24 16:55:41 +0200

    updated ExerciseSet03.java

--- src/ExerciseSet03.java
+++ src/ExerciseSet03.java
@@ -29,55 +29,42 @@ public class ExerciseSet03 {
         System.out.println(u+ " EUR gesamtkosten");
     }
     public static int getDaysOfMonth(int month, int year){
-
-          int days= 0;
-        switch (month){
-            case 0:
-                System.out.println("wrong argument");
-                break;
-            case 1:
-                days= 29;
-                System.out.println("es waren 29 tage");
-            case 2:
-                days= 28;
-                if ( year%4==0){
-                    System.out.println("es waren 29 tage");
-                } else
-                System.out.println("es waren 28 tage");
-            break;
-            case 3:
-            case 5:
-            case 8:
-            case 12:
-                days= 31;
-                System.out.println("es waren 31 tage");
-                break;
-            case 4:
-            case 11:
-            case 6:
-            case 7:
-            case 9:
-                days= 30;
-                System.out.println("es waren 30 tage");
-                break;
-            case 10:
-                days= 29;
-                System.out.println("es waren 29 tage");
-                break;
+        if (month < 8) {
+            if (month == 2) {
+                if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+                    return 29;
+                } else {
+                    return 28;
+                }
+            } else {
+                if (month % 2 == 0) {
+                    return 30;
+                } else {
+                    return 31;
+                }
+            }
+        } else {
+            if (month % 2 == 0) {
+                return 31;
+            } else {
+                return 30;
+            }
         }
-return days;
     }
-    public static int getDayOfTheWeek
-            (int day, int month, int year) {
-          int wochentag= ((day+((month+1)*13)/5)%+(year%100)+((year%100)/4)%+(((year- (year%100))/100)/4)%-2*((year- (year%100))/100))%7;
-        if (wochentag==0) wochentag=7;
+
+    public static int getDayOfTheWeek(int day, int month, int year)
+    {
+        int wochentag = ((day + ((month + 1) * 13) / 5) % +(year % 100) + ((year % 100) / 4) % +(((year - (year % 100)) / 100) / 4) % -2 * ((year - (year % 100)) / 100)) % 7;
+        if (wochentag == 0) wochentag = 7;
         System.out.println(wochentag);
-          return wochentag;
+        return wochentag;
     }
-    public static void printCalendar(int month, int year){
 
-                for (int i=0; i<=getDaysOfMonth(month,year); i++)
-                System.out.println(i);
-        }
+    public static void printCalendar(int month, int year)
+    {
+        for (int i = 1; i <= getDaysOfMonth(month, year); i++)
+            if (i % 7 != 0) System.out.print(i + " ");
+            else System.out.println(i);
     }
+}
 
