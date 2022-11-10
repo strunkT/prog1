d1f8544617c92bd8ea923153a381300da9f49d30
bircni <nicolas@bircks.eu>
2022-11-10 15:54:29 +0100

    added new method

--- src/ExerciseSet06.java
+++ src/ExerciseSet06.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class ExerciseSet06 {
 
     public static void main(String[] args) {
-random();
+
+        zufall();
     }
     public static int random(){
         int size= Terminal.readInt("size");
@@ -13,13 +14,28 @@ random();
         int [] ran= new int[size];
         Random zufall= new Random();
 
-        for (int i=0; i<size; i++) {
+        for (int i = 0; i < size; i++) {
             while (i > min && i < max) {
                 ran[i] = zufall.nextInt(min, max);
                 System.out.println("Array:" + ran[i]);
 
-            } return ran[i];
+            }
+            return ran[i];
         }
-       return random();
+        //dauerschleife - methode ruft unendlich oft sich selbst auf
+        return random();
+    }
+
+    public static int[] zufall()
+    {
+        int size = Terminal.readInt("size");
+        int min = Terminal.readInt("min");
+        int max = Terminal.readInt("max");
+        int[] array = new int[size];
+        for (int i = 0; i <= size - 1; i++) {
+            array[i] = min + (int) (Math.random() * ((max - min) + 1));
+            System.out.println(array[i]);
+        }
+        return array;
     }
 }
