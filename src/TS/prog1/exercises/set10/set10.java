f83c8f46dbd3f40d16a153d0d7da43f0969cf53e
bircni <nicolas@bircks.eu>
2022-12-07 14:51:31 +0100

    update

--- src/TS/prog1/exercises/set10/set10.java
+++ src/TS/prog1/exercises/set10/set10.java
@@ -3,15 +3,23 @@ package TS.prog1.exercises.set10;
 public class set10 {
     public static double fakultaet(int fak){
         double res=0;
-        if (fak==0) {
+        if (fak == 0) {
             res = 1;
-        }      else{ return fak* fakultaet(fak-1);}
+        } else {
+            return fak * fakultaet(fak - 1);
+        }
 
-        System.out.println(res);
+        //System.out.println(res);
         return res;
     }
 
-    public static void main(String[] args) {
-        fakultaet(3);
+    public static void main(String[] args)
+    {
+        System.out.println(fakultaet(1));
+        System.out.println(fakultaet(2));
+        System.out.println(fakultaet(3));
+        System.out.println(fakultaet(4));
+
     }
+
 }
